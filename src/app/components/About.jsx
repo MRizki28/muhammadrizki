eb5e5a036fe3271cf9fae37aafbdea5d6f75926f
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-17 01:43:55 +0800

    update image

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -18,10 +18,11 @@ const AboutBase = () => {
                     <Image src="/assets/img/me.jpeg" alt="" width={500} height={300} className="max-w-sm w-[50%] md:w-[45%] lg:w-full rounded-lg shadow-2xl" data-aos="flip-left" data-aos-duration="1000" />
                     <div className="ml-0 2xl:ml-[180px] text-center sm:text-center md:text-center lg:text-left">
                         <div className="mb-3">
-                            <h1 className="text-[25px] sm:text-[32px] lg:text-[32px] xl:text-[32px]  font-poppins-reguler font-bold " data-aos="fade-down-right" data-aos-duration="1000">Hello<FontAwesomeIcon icon={faHand} className="fa-shake fa-sm pb-[13px] text-[#E2C799]" /><span className="text-[#9400FF] ">I'm Muhammad Rizki</span></h1>
+                        <h1 className="text-[25px] sm:text-[32px] lg:text-[32px] xl:text-[32px]  font-poppins-reguler font-bold " data-aos="fade-down-right" data-aos-duration="1000">Hello<FontAwesomeIcon icon={faHand} className="fa-shake fa-sm pb-[13px] text-[#E2C799]" /><span className="text-[#9400FF] ">I&apos;m Muhammad Rizki</span></h1>
+
                         </div>
                         <div data-aos="fade-down-right" data-aos-duration="1200">
-                            <h1 className="text-[20px] sm:text-[30px] lg:text-[30px] xl:text-[30px] fontcontainer mx-auto  font-poppins-reguler ">A <span className="text-[#9400FF]">Fullstack Developer</span>Based in <span className="text-[#9400FF]">indonesia</span> </h1>
+                            <h1 className="text-[20px] sm:text-[30px] lg:text-[30px] xl:text-[30px] fontcontainer mx-auto  font-poppins-reguler ">A <span className="text-[#9400FF]">Fullstack Developer</span> 🧑‍💻 Based in <span className="text-[#9400FF]">indonesia</span>  🌏</h1>
                         </div>
                         <div data-aos="zoom-in-up" data-aos-duration="1500">
                             <p className="py-6 text-[16px] sm:text-[18px] lg:text-[18px] xl:text-[18px] ">Hello everyone, I am Muhammad Rizki, I am a software developer. I like learning new things, especially in the field of technology and I also like challenges. I often work with other developer teams</p>
