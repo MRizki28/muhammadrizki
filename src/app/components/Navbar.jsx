a33d7729d02a5625aca681c0ae087d78b126bd3a
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-17 10:04:23 +0800

    update

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
 
     const navList = (
 
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <ul className="mb-4 mt-2 mr-[138px] text-white flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
             <Typography
                 as="li"
                 variant="small"
@@ -68,20 +68,20 @@ const NavBar = () => {
 
     return (
         <Navbar className="fixed 2xl:p-[30px] max-w-screen-3xl top-0 w-full z-50 py-2 px-4 lg:px-8 lg:py-4 rounded-none border-0 navbar-color bg-transparent shadow-md">
-            <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+            <div className="container mx-auto flex items-center justify-between text-blue-gray-900 text-[#9400FF]">
                 <Typography
                     as="a"
                     href="#"
-                    className="mr-4 cursor-pointer py-1.5 text-[24px] font-medium font-poppins-reguler"
+                    className="mr-4 cursor-pointer py-1.5 text-[24px] font-medium  text-sd  font-poppins-reguler"
                 >
                     Muhammad Rizki
                 </Typography>
-                <div className="hidden lg:block">{navList}</div>
+                <div className="hidden  lg:block">{navList}</div>
                 <Typography
                         as="li"
                         variant="small"
                         color="blue-gray"
-                        className="p-1 font-normal text-[16px] font-poppins-reguler hidden lg:inline-block"
+                        className="p-1 font-normal text-white text-[16px] font-poppins-reguler hidden lg:inline-block"
                     >
                         <a href="#" className="flex items-center">
                         <FontAwesomeIcon icon={faLinkedin}> </FontAwesomeIcon>
@@ -91,7 +91,7 @@ const NavBar = () => {
                     </Typography>
                 <IconButton
                     variant="text"
-                    className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                    className="ml-auto t h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
                     ripple={false}
                     onClick={() => setOpenNav(!openNav)}
                 >
@@ -99,7 +99,7 @@ const NavBar = () => {
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
-                            className="h-6 w-6"
+                            className="h-6 w-6 text-white"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                             strokeWidth={2}
@@ -113,7 +113,7 @@ const NavBar = () => {
                     ) : (
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
+                            className="h-6 w-6 text-white"
                             fill="none"
                             stroke="currentColor"
                             strokeWidth={2}
@@ -135,7 +135,7 @@ const NavBar = () => {
                         as="li"
                         variant="small"
                         color="blue-gray"
-                        className="p-1 font-normal text-[16px] font-poppins-reguler"
+                        className="p-1 text-white font-normal text-[16px] font-poppins-reguler"
                     >
                        
                        <a href="#" className="flex items-center">
