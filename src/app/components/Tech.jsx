65fc20e1e33fe1523efc5e5b87ed02de451b05cc
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-17 10:17:50 +0800

    update

--- src/app/components/Tech.jsx
+++ src/app/components/Tech.jsx
@@ -12,64 +12,64 @@ const TechBase = () => {
     return (
         <div id="tech" className="tech grid min-h-[43vh]  bg-[#050810]">
             <h2 className="text-center mt-[47px] mb-16 text-[2.5rem] text-sd font-poppins-reguler text-white">Tooling</h2>
-            <div className="grid grid-cols-2 container mx-auto lg:max-w-[77%] xl:max-w-[89%] 2xl:max-w-[77%]">
-                <div className="grid grid-cols-1 mb-[63px] md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-6 mt-10 container mx-auto items-center lg:mb-[94px] border-r-4">
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1000">
-                        <img src="assets/img/tech/laravel.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+            <div className="grid grid-cols-1 md:grid-cols-2 max-w-[90%] container mx-auto md:max-w-[90%] lg:max-w-[77%] xl:max-w-[89%] 2xl:max-w-[77%]">
+                <div className="grid grid-cols-2 mb-[63px] md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-6 mt-10 container mx-auto items-center lg:mb-[94px] border-b-4 md:border-b-0 md:border-r-4">
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1000">
+                        <img src="assets/img/tech/laravel.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Laravel</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="1200">
-                        <img src="assets/img/tech/lumen.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="1200">
+                        <img src="assets/img/tech/lumen.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Lumen</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1400">
-                        <img src="assets/img/tech/ci.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1400">
+                        <img src="assets/img/tech/ci.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Codeigniter</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="1600">
-                        <img src="assets/img/tech/react.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="1600">
+                        <img src="assets/img/tech/react.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">React.Js</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1800">
-                        <img src="assets/img/tech/express.png" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1800">
+                        <img src="assets/img/tech/express.png" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Express.Js</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="2000">
-                        <img src="assets/img/tech/nextjs.png" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="2000">
+                        <img src="assets/img/tech/nextjs.png" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Next.Js</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="2200">
-                        <img src="assets/img/tech/tailwind-css.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="2200">
+                        <img src="assets/img/tech/tailwind-css.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Talwinds</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="2200">
-                        <img src="assets/img/tech/bootstrap.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="2200">
+                        <img src="assets/img/tech/bootstrap.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">Bootstrap</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="2200">
-                        <img src="assets/img/tech/react.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="2200">
+                        <img src="assets/img/tech/react.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">React Native</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="1000">
-                        <img src="assets/img/tech/php.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-down" data-aos-duration="1000">
+                        <img src="assets/img/tech/php.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">PHP</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1000">
-                        <img src="assets/img/tech/javascript.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1000">
+                        <img src="assets/img/tech/javascript.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">JavaScript</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-cown" data-aos-duration="1000">
-                        <img src="assets/img/tech/restapi.png" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-cown" data-aos-duration="1000">
+                        <img src="assets/img/tech/restapi.png" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">REST API</span>
                     </div>
-                    <div className="bg-transparent  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1000">
-                        <img src="assets/img/tech/mysql.svg" alt="" className="mr-4 w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
+                    <div className="bg-transparent justify-center  p-4 flex items-center"  data-aos="fade-up" data-aos-duration="1000">
+                        <img src="assets/img/tech/mysql.svg" alt="" className="mr-4 w-[8%] md:w-[20%] aspect-[3/4] object-contai transition-transform transform hover:scale-105 hover:shadow-lg" />
                         <span className="text-xl text-white font-poppins-reguler">MySQL</span>
                     </div>
                 </div>
-                <div className="text-center my-32 md:my-28 md:ml-[79px] xl:ml-6 xl:my-28" data-aos="flip-left" data-aos-duration="1000" >
+                <div className="text-center mb-[100px] my-18 md:my-28 md:ml-[79px] xl:ml-6 xl:my-28" data-aos="flip-left" data-aos-duration="1000" >
                     <div className=" mt-14">
-                        <span className="text-[50px] font-bold font-poppins-reguler text-center text-sd text-[#9400FF]">
+                        <span className="text-[40px] md:text-[50px] font-bold font-poppins-reguler text-center text-sd text-[#9400FF]">
                             <ReactTypingEffect speed={100} text={['Tech Stack⚙️ & Tools🛠️']} />
                         </span>
                     </div>
