ddf2fb44d378eb68c7b85aaf19ca0c7555d26377
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-19 07:26:53 +0800

    update education

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import HeroBase from './components/Hero'
 import AboutBase from './components/About'
 import PageUpButton from './components/componentslist/Floating'
 import TechBase from './components/Tech'
+import Education from './components/Education'
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <AboutBase></AboutBase>
       <PageUpButton></PageUpButton>
       <TechBase></TechBase>
+      <Education></Education>
     </>
 
   )
